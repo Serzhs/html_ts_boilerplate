d9448e4288af2093645847c97a0b6e41b399b5bc
Jānis Seržants <serzants.janis@gmail.com>
2023-02-01 13:45:38 +0200

    Added jQuery and added import in sample

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,1 +1,8 @@
+import $ from 'jquery';
+import sum from './utils/sum/sum';
+
 console.log('Ready for coding');
+
+console.log('Body jQuery node:', $('body'));
+console.log('Body javascript node:', document.querySelector('body'));
+console.log('2 + 3 =', sum(2, 3));
